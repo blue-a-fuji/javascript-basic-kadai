010d1514f6da18f0320b27f7b541cacebc878c68
blue-a-fuji <chocolate.akko54@gmail.com>
2025-04-15 23:45:39 +0900

    kadai_mdn_jsフォルダの削除

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const today = new Date("2025,4,15");
-const year = today.getFullYear();
-const month = today.getMonth() + 1;
-const date = today.getDate();
-
-console.log(year + '年'+ month + '月' + date +'日');
-
-
-
